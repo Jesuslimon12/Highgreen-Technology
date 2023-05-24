a40fa4ab9329d3a3f66bcbb7a3da7fb0a1e29943
Jesuslimon12 <cris1630338@gmail.com>
2023-05-24 15:37:46 -0600

    Actualización 2.0

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,8 +9,8 @@ export default function About() {
     <div className='about' id='about'>
       <Main_title bgtitle={'WHO WE ARE'} title={'Highgreen Technology'} letter_spacing={'primary'} font_size='normal'/>
       <p className='main-text'>At Highgreen Technology, we are revolutionizing the cleaning industry with our high-performance compact sweepers powered by <span className='main-text--active'>Haaga's patented belt-less disk brush technology.</span> 
-      <br /> Say goodbye to messy and unreliable belt-driven sweepers and enjoy outstanding cleaning results with ease of use and minimal maintenance. Whether you're a small business or a factory,
-      our sweepers will provide a better cleaning experience for your organization. </p>
+      <br /> Say goodbye to messy and unreliable belt-driven sweepers and enjoy outstanding cleaning results with ease of use and minimal maintenance. <u> Whether you're a small business or a factory,
+      our sweepers will provide a better cleaning experience for your organization.</u>  </p>
 
       <figcaption>
         <img src={IconLighter} alt="" />
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -32,7 +32,6 @@ export default function Footer() {
       
   
         
-        
 
     </footer>
   )
